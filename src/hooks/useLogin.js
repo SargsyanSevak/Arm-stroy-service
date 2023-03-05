24ad5678674a761654cd5bbdfcad20e83497ba14
sargsyansevak <thesevaksargsyan@gmail.com>
2023-03-05 13:19:57 +0400

    dsds

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { useAuthContext } from './useAuthContext'
 import Cookies from 'js-cookie';
+import { instance } from '../axios';
 
 export const useLogin = () => {
   const [error, setError] = useState(null)
@@ -10,31 +11,41 @@ export const useLogin = () => {
   const login = async (email, password) => {
     setIsLoading(true)
     setError(null)
-
-    const response = await fetch('/api/user/login', {
-      method: 'POST',
-      headers: {'Content-Type': 'application/json',
-      "Access-Control-Allow-Origin" : "*", 
-      "Access-Control-Allow-Credentials" : true 
-    },
-      body: JSON.stringify({email, password })
+    instance
+    .post('api/user/login',{email, password })
+    .then(res =>{
+      if (res.status === 400) {
+        setIsLoading(false)
+        setError(res.data.error)
+      }
+      localStorage.setItem('user', JSON.stringify(res.data))
+      dispatch({type: 'LOGIN', payload: res.data})
+      setIsLoading(false)
     })
-    const json = await response.json()
+    // const response = await fetch('/api/user/login', {
+    //   method: 'POST',
+    //   headers: {'Content-Type': 'application/json',
+    //   "Access-Control-Allow-Origin" : "*", 
+    //   "Access-Control-Allow-Credentials" : true 
+    // },
+    //   body: JSON.stringify({email, password })
+    // })
+    // const json = await response.json()
 
-    if (!response.ok) {
-      setIsLoading(false)
-      setError(json.error)
-    }
-    if (response.ok) {
-      // save the user to local storage
-      localStorage.setItem('user', JSON.stringify(json))
+    // if (!response.ok) {
+    //   setIsLoading(false)
+    //   setError(json.error)
+    // }
+    // if (response.ok) {
+    //   // save the user to local storage
+    //   localStorage.setItem('user', JSON.stringify(json))
 
-      // update the auth context
-      dispatch({type: 'LOGIN', payload: json})
+    //   // update the auth context
+    //   dispatch({type: 'LOGIN', payload: json})
 
-      // update loading state
-      setIsLoading(false)
-    }
+    //   // update loading state
+    //   setIsLoading(false)
+    // }
   }
 
   return { login, isLoading, error }
